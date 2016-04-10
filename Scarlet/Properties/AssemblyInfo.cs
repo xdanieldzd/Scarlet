6947bb6edb508a4520e126be864099096bc473d9
xdaniel <xdanieldzd@users.noreply.github.com>
2016-04-10 23:52:33 +0200

    Bumped Scarlet version number

--- Scarlet/Properties/AssemblyInfo.cs
+++ Scarlet/Properties/AssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("3f27160a-8f2b-48a8-8dd2-77126e7547d2")]
 
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyVersion("0.5.1.0")]
+[assembly: AssemblyFileVersion("0.5.1.0")]
 
 [assembly: InternalsVisibleTo("Scarlet.IO.ImageFormats")]
 [assembly: InternalsVisibleTo("Scarlet.IO.ContainerFormats")]
