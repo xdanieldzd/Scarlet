819284b63c4d2244e6477c339195e3074f561bf6
xdaniel <xdanieldzd@users.noreply.github.com>
2016-11-10 20:38:23 +0100

    Added preliminary Stellvia .dat and TIPS support
    Added preliminary support for .dat archives and TIPS images from Uchuu no Stellvia for PS2

---
+++ Scarlet.IO.ContainerFormats/DATStellvia.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Scarlet.IO.ContainerFormats
+{
+    public class DATStellviaFile : ContainerElement
+    {
+        DATStellvia parentContainer;
+
+        public uint SectorNumber { get; private set; }
+        public uint DataSize { get; private set; }
+        public string FilePath { get; private set; }
+
+        public DATStellviaFile(EndianBinaryReader reader, DATStellvia container)
+        {
+            parentContainer = container;
+
+            SectorNumber = reader.ReadUInt32();
+            DataSize = reader.ReadUInt32();
+            FilePath = Encoding.GetEncoding(932).GetString(reader.ReadBytes(0x38)).TrimEnd('\0');
+        }
+
+        public override string GetName()
+        {
+            return FilePath;
+        }
+
+        public override Stream GetStream(Stream containerStream)
+        {
+            containerStream.Seek(SectorNumber * 0x800, SeekOrigin.Begin);
+            MemoryStream stream = new MemoryStream();
+            FileFormat.CopyStream(containerStream, stream, (int)DataSize);
+            stream.Seek(0, SeekOrigin.Begin);
+            return stream;
+        }
+    }
+
+    // TODO: horrible horrible pattern, but, uh, not much else I can do, really...
+    [FilenamePattern("^.*data\\d+\\.dat$")]
+    public class DATStellvia : ContainerFormat
+    {
+        public DATStellviaFile[] Files { get; private set; }
+
+        protected override void OnOpen(EndianBinaryReader reader)
+        {
+            reader.Endianness = Endian.LittleEndian;
+
+            List<DATStellviaFile> files = new List<DATStellviaFile>();
+            uint check = uint.MaxValue;
+            while (check != 0)
+            {
+                check = reader.ReadUInt32();
+                reader.BaseStream.Seek(-4, SeekOrigin.Current);
+                if (check == 0) break;
+                files.Add(new DATStellviaFile(reader, this));
+            }
+            Files = files.ToArray();
+        }
+
+        public override int GetElementCount()
+        {
+            return (int)Files.Length;
+        }
+
+        protected override ContainerElement GetElement(Stream containerStream, int elementIndex)
+        {
+            if (elementIndex < 0 || elementIndex >= Files.Length) throw new IndexOutOfRangeException("Invalid file index specified");
+            return Files[elementIndex];
+        }
+    }
+}
---
+++ Scarlet.IO.ImageFormats/TIPS.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+
+using Scarlet.Drawing;
+using Scarlet.IO;
+using Scarlet.Platform.Sony;
+
+namespace Scarlet.IO.ImageFormats
+{
+    // TODO: figure out the rest of this stuff? like if eye/mouth/etc positioning is in here or elsewhere...
+
+    [FilenamePattern("^.*\\.tips$")]
+    public class TIPS : ImageFormat
+    {
+        public uint Unknown0x00 { get; private set; }   // 40000000
+        public uint Unknown0x04 { get; private set; }   // 0FC5BD43
+        public uint FrameDataOffset { get; private set; }
+        public uint PixelDataOffset { get; private set; }
+        public ushort RawImageWidth { get; private set; }
+        public ushort RawImageHeight { get; private set; }
+        public ushort NumImageInformation { get; private set; }
+        public ushort Unknown0x16 { get; private set; }
+        public ushort Unknown0x18 { get; private set; }
+        public ushort Unknown0x1A { get; private set; }
+        public uint Unknown0x1C { get; private set; }
+        public uint Unknown0x20 { get; private set; }
+        public uint Unknown0x24 { get; private set; }
+        public uint Unknown0x28 { get; private set; }
+        public uint Unknown0x2C { get; private set; }
+        public uint Unknown0x30 { get; private set; }
+        public uint Unknown0x34 { get; private set; }
+        public uint Unknown0x38 { get; private set; }
+        public uint Unknown0x3C { get; private set; }
+
+        public TIPSImageInfo[] ImageInfos { get; private set; }
+        public TIPSRectangleInfo[][] RectInfos { get; private set; }
+
+        public byte[] PixelData { get; private set; }
+
+        Bitmap rawImage;
+
+        protected override void OnOpen(EndianBinaryReader reader)
+        {
+            /* Read header(?) */
+            Unknown0x00 = reader.ReadUInt32();
+            Unknown0x04 = reader.ReadUInt32();
+            FrameDataOffset = reader.ReadUInt32();
+            PixelDataOffset = reader.ReadUInt32();
+            RawImageWidth = reader.ReadUInt16();
+            RawImageHeight = reader.ReadUInt16();
+            NumImageInformation = reader.ReadUInt16();
+            Unknown0x16 = reader.ReadUInt16();
+            Unknown0x18 = reader.ReadUInt16();
+            Unknown0x1A = reader.ReadUInt16();
+            Unknown0x1C = reader.ReadUInt32();
+            Unknown0x20 = reader.ReadUInt32();
+            Unknown0x24 = reader.ReadUInt32();
+            Unknown0x28 = reader.ReadUInt32();
+            Unknown0x2C = reader.ReadUInt32();
+            Unknown0x30 = reader.ReadUInt32();
+            Unknown0x34 = reader.ReadUInt32();
+            Unknown0x38 = reader.ReadUInt32();
+            Unknown0x3C = reader.ReadUInt32();
+
+            /* Get image information */
+            ImageInfos = new TIPSImageInfo[NumImageInformation];
+            for (int i = 0; i < NumImageInformation; i++)
+                ImageInfos[i] = new TIPSImageInfo(reader);
+
+            /* Get rect information */
+            RectInfos = new TIPSRectangleInfo[NumImageInformation][];
+            for (int i = 0; i < NumImageInformation; i++)
+            {
+                TIPSImageInfo imageInfo = ImageInfos[i];
+
+                long position = reader.BaseStream.Position;
+                reader.BaseStream.Seek(imageInfo.RectOffset, SeekOrigin.Begin);
+
+                RectInfos[i] = new TIPSRectangleInfo[imageInfo.NumRects];
+                for (int j = 0; j < imageInfo.NumRects; j++)
+                    RectInfos[i][j] = new TIPSRectangleInfo(reader);
+
+                reader.BaseStream.Seek(position, SeekOrigin.Begin);
+            }
+
+            /* Get pixel data */
+            reader.BaseStream.Seek(PixelDataOffset, SeekOrigin.Begin);
+            PixelData = reader.ReadBytes((int)(reader.BaseStream.Length - PixelDataOffset));
+
+            /* Scale alpha */
+            for (int i = 0; i < PixelData.Length; i += 4)
+                PixelData[i + 3] = PS2.ScaleAlpha(PixelData[i + 3]);
+
+            /* Create raw image bitmap */
+            ImageBinary rawImageBinary = new ImageBinary();
+            rawImageBinary.Width = RawImageWidth;
+            rawImageBinary.Height = RawImageHeight;
+            rawImageBinary.InputPixelFormat = PixelDataFormat.FormatAbgr8888;
+            rawImageBinary.AddInputPixels(PixelData);
+            rawImage = rawImageBinary.GetBitmap();
+        }
+
+        public override int GetImageCount()
+        {
+            return NumImageInformation;
+        }
+
+        public override int GetPaletteCount()
+        {
+            return 1;
+        }
+
+        protected override Bitmap OnGetBitmap(int imageIndex, int paletteIndex)
+        {
+            TIPSImageInfo imageInfo = ImageInfos[imageIndex];
+            TIPSRectangleInfo[] rectInfos = RectInfos[imageIndex];
+
+            Bitmap image = new Bitmap(imageInfo.Width, imageInfo.Height);
+            using (Graphics g = Graphics.FromImage(image))
+            {
+                for (int i = 0; i < rectInfos.Length; i++)
+                {
+                    TIPSRectangleInfo rectInfo = rectInfos[i];
+                    Rectangle sourceRect = new Rectangle(rectInfo.SourceX * 32, rectInfo.SourceY * 32, (int)rectInfo.SourceWidth * 32, 32);
+                    g.DrawImage(rawImage, rectInfo.TargetX * 32, rectInfo.TargetY * 32, sourceRect, GraphicsUnit.Pixel);
+                }
+            }
+            return image;
+
+        }
+    }
+
+    public class TIPSImageInfo
+    {
+        public uint Unknown0x00 { get; private set; }   // 40000000
+        public uint Unknown0x04 { get; private set; }   // 0FC5BD43
+        public ushort Width { get; private set; }
+        public ushort Height { get; private set; }
+        public ushort Unknown0x0C { get; private set; }
+        public ushort Unknown0x0E { get; private set; }
+        public uint RectOffset { get; private set; }
+        public uint NumRects { get; private set; }
+        public ushort Unknown0x18 { get; private set; }
+        public ushort Unknown0x1A { get; private set; }
+        public uint Unknown0x1C { get; private set; }
+        public uint Unknown0x20 { get; private set; }
+        public uint Unknown0x24 { get; private set; }
+        public uint Unknown0x28 { get; private set; }
+        public uint Unknown0x2C { get; private set; }
+        public uint Unknown0x30 { get; private set; }
+        public uint Unknown0x34 { get; private set; }
+        public uint Unknown0x38 { get; private set; }
+        public uint Unknown0x3C { get; private set; }
+
+        public TIPSImageInfo(EndianBinaryReader reader)
+        {
+            Unknown0x00 = reader.ReadUInt32();
+            Unknown0x04 = reader.ReadUInt32();
+            Width = reader.ReadUInt16();
+            Height = reader.ReadUInt16();
+            Unknown0x0C = reader.ReadUInt16();
+            Unknown0x0E = reader.ReadUInt16();
+            RectOffset = reader.ReadUInt32();
+            NumRects = reader.ReadUInt32();
+            Unknown0x18 = reader.ReadUInt16();
+            Unknown0x1A = reader.ReadUInt16();
+            Unknown0x1C = reader.ReadUInt32();
+            Unknown0x20 = reader.ReadUInt32();
+            Unknown0x24 = reader.ReadUInt32();
+            Unknown0x28 = reader.ReadUInt32();
+            Unknown0x2C = reader.ReadUInt32();
+            Unknown0x30 = reader.ReadUInt32();
+            Unknown0x34 = reader.ReadUInt32();
+            Unknown0x38 = reader.ReadUInt32();
+            Unknown0x3C = reader.ReadUInt32();
+        }
+    }
+
+    public class TIPSRectangleInfo
+    {
+        /* 32px blocks! */
+        public byte TargetX { get; private set; }
+        public byte TargetY { get; private set; }
+        public byte SourceX { get; private set; }
+        public byte SourceY { get; private set; }
+        public uint SourceWidth { get; private set; }
+
+        public TIPSRectangleInfo(EndianBinaryReader reader)
+        {
+            TargetX = reader.ReadByte();
+            TargetY = reader.ReadByte();
+            SourceX = reader.ReadByte();
+            SourceY = reader.ReadByte();
+            SourceWidth = reader.ReadUInt32();
+        }
+    }
+}
