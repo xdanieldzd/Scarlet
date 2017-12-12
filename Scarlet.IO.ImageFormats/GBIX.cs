600ab4c36267082eb59ecd7420433def1decb0a7
xdaniel <xdanieldzd@users.noreply.github.com>
2017-12-12 18:18:35 +0100

    Improved GBIX support
    Improved support for GBIX images (ex. K-ON! Houkago Live); still missing some details, but most files should convert properly now

--- Scarlet.IO.ImageFormats/GBIX.cs
+++ Scarlet.IO.ImageFormats/GBIX.cs
@@ -10,11 +10,77 @@ using Scarlet.IO;
 
 namespace Scarlet.IO.ImageFormats
 {
-    // TODO: verify me!
+    // TODO: more imagetypes, verify mipmap stuff, figure out unknown DXT-ish datatype, etc.
+
+    // "E:\[SSD User Data]\Desktop\Misc Stuff\PSP K-ON Test\PSP_GAME" -keep
+    // "E:\[SSD User Data]\Desktop\Misc Stuff\PSP K-ON Test\Unimplemented stuff test"
+
+    public enum GbixDataType : byte
+    {
+        Abgr1555 = 0x00,
+        Bgr565 = 0x01,
+        Abgr4444 = 0x02,
+        Abgr8888 = 0x03,
+
+        DXT1Rgba = 0x0A,
+        DXT_Unknown = 0x0C,
+    }
+
+    public enum GbixImageType : byte
+    {
+        Linear = 0x80,
+        LinearMipmaps = 0x81,
+        // Unknown? 0x82
+        // Unknown w/ mipmaps? 0x83
+        // Unknown2? 0x84
+        // Unknown2 w/ mipmaps? 0x85
+        Indexed4 = 0x86,
+        // Indexed4 w/ mipmaps? 0x87
+        Indexed4_Alt = 0x88,
+        // Indexed4 alt w/ mipmaps? 0x89
+        Indexed8 = 0x8A,
+        // Indexed8 w/ mipmaps? 0x8B
+        Indexed8_Alt = 0x8C,
+        // Indexed8 alt w/ mipmaps? 0x8D
+        // Unknown3? 0x8E
+        // Unknown3 w/ mipmaps? 0x8F
+    }
+
+    internal class GbixMipmapInfo
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public byte[] PixelData { get; private set; }
+
+        public GbixMipmapInfo(int width, int height, byte[] data)
+        {
+            Width = width;
+            Height = height;
+            PixelData = data;
+        }
+    }
 
     [MagicNumber("GBIX", 0x00)]
     public class GBIX : ImageFormat
     {
+        static readonly Dictionary<GbixDataType, int> dataTypeSizeMap = new Dictionary<GbixDataType, int>()
+        {
+            { GbixDataType.Abgr1555, 16 },
+            { GbixDataType.Bgr565, 16 },
+            { GbixDataType.Abgr4444, 16 },
+            { GbixDataType.Abgr8888, 32 },
+        };
+
+        static readonly Dictionary<GbixDataType, PixelDataFormat> dataTypeFormatMap = new Dictionary<GbixDataType, PixelDataFormat>()
+        {
+            { GbixDataType.Abgr1555, PixelDataFormat.FormatAbgr1555 },
+            { GbixDataType.Bgr565, PixelDataFormat.FormatBgr565 },
+            { GbixDataType.Abgr4444, PixelDataFormat.FormatAbgr4444 },
+            { GbixDataType.Abgr8888, PixelDataFormat.FormatAbgr8888 },
+            { GbixDataType.DXT1Rgba, PixelDataFormat.FormatDXT1Rgba_PSP },
+            //{ GbixDataType.DXT_Unknown, PixelDataFormat.FormatDXT3 },         // TODO: disabled for now, better no image than a garbage image imo
+        };
+
         public string MagicNumber { get; private set; }
         public uint Unknown0x04 { get; private set; }
         public uint Unknown0x08 { get; private set; }
@@ -22,12 +88,18 @@ namespace Scarlet.IO.ImageFormats
 
         public string MagicNumberUVRT { get; private set; }
         public uint UVRTDataSize { get; private set; }
-        public uint Format { get; private set; } // Maybe bitfield?
+        public GbixDataType DataType { get; private set; }
+        public GbixImageType ImageType { get; private set; }
+        public ushort Unknown0x1A { get; private set; }
         public ushort Width { get; private set; }
         public ushort Height { get; private set; }
 
         public byte[] PaletteData { get; private set; }
         public byte[] PixelData { get; private set; }
+
+        bool isIndexed, isCompressed, hasMipmaps;
+
+        List<GbixMipmapInfo> mipmapData;
 
         protected override void OnOpen(EndianBinaryReader reader)
         {
@@ -38,34 +110,79 @@ namespace Scarlet.IO.ImageFormats
 
             MagicNumberUVRT = Encoding.ASCII.GetString(reader.ReadBytes(4));
             UVRTDataSize = reader.ReadUInt32();
-            Format = reader.ReadUInt32();
+            DataType = (GbixDataType)reader.ReadByte();
+            ImageType = (GbixImageType)reader.ReadByte();
+            Unknown0x1A = reader.ReadUInt16();
             Width = reader.ReadUInt16();
             Height = reader.ReadUInt16();
 
-            if (Format == 0x8602)
-                PaletteData = reader.ReadBytes(16 * 2);
-            else if (Format == 0x8803)
-                PaletteData = reader.ReadBytes(16 * 4);
-            else if (Format == 0x8A02)
-                PaletteData = reader.ReadBytes(256 * 2);
-            else if (Format == 0x8C03)
-                PaletteData = reader.ReadBytes(256 * 4);
-            else if (Format == 0x800A)
-                PaletteData = new byte[0];
-            else
-                throw new NotImplementedException();
+            // TODO: more conditions once more values for ImageType are known!
+            isIndexed = (ImageType != GbixImageType.Linear && ImageType != GbixImageType.LinearMipmaps);
+            isCompressed = ((ImageType == GbixImageType.Linear || ImageType == GbixImageType.LinearMipmaps) && (DataType == GbixDataType.DXT1Rgba || DataType == GbixDataType.DXT_Unknown));
+            hasMipmaps = (ImageType == GbixImageType.LinearMipmaps);
 
-            PixelData = reader.ReadBytes((int)(UVRTDataSize - 8 - PaletteData.Length));
+            if (isIndexed)
+            {
+                if (!dataTypeSizeMap.ContainsKey(DataType))
+                    throw new NotImplementedException(string.Format("GBIX: unimplemented palette data size for {0} (0x{1:X2})", DataType, (byte)DataType));
+
+                switch (ImageType)
+                {
+                    case GbixImageType.Indexed4:
+                    case GbixImageType.Indexed4_Alt:
+                        PaletteData = reader.ReadBytes(16 * (dataTypeSizeMap[DataType] / 8));
+                        break;
+
+                    case GbixImageType.Indexed8:
+                    case GbixImageType.Indexed8_Alt:
+                        PaletteData = reader.ReadBytes(256 * (dataTypeSizeMap[DataType] / 8));
+                        break;
+
+                    default:
+                        throw new NotImplementedException(string.Format("GBIX: unimplemented indexed image type {0} (0x{1:X2})", ImageType, (byte)ImageType));
+                }
+            }
+
+            PixelData = reader.ReadBytes((int)(UVRTDataSize - 8 - (PaletteData != null ? PaletteData.Length : 0)));
+
+            mipmapData = new List<GbixMipmapInfo>();
+            if (hasMipmaps)
+            {
+                int mipWidth = Width;
+                int mipHeight = Height;
+                int mipCount = 0, readOffset = 0;
+
+                int bitsPerPixel = dataTypeSizeMap[DataType];
+
+                while (mipWidth > 0 && mipHeight > 0 && readOffset < PixelData.Length)
+                {
+                    mipWidth = (Width >> mipCount);
+                    mipHeight = (Height >> mipCount);
+                    int mipDataSize = (((bitsPerPixel * (Width >> mipCount)) * (Height >> mipCount)) / 8);
+
+                    byte[] mipPixelData = new byte[mipDataSize];
+                    Buffer.BlockCopy(PixelData, readOffset, mipPixelData, 0, mipPixelData.Length);
+                    readOffset += mipPixelData.Length;
+
+                    mipmapData.Add(new GbixMipmapInfo(mipWidth, mipHeight, mipPixelData));
+
+                    mipCount++;
+                }
+            }
+            else
+            {
+                mipmapData.Add(new GbixMipmapInfo(Width, Height, PixelData));
+            }
         }
 
         public override int GetImageCount()
         {
-            return 1;
+            return mipmapData.Count;
         }
 
         public override int GetPaletteCount()
         {
-            return (PaletteData.Length > 0 ? 1 : 0);
+            return (PaletteData != null ? 1 : 0);
         }
 
         protected override Bitmap OnGetBitmap(int imageIndex, int paletteIndex)
@@ -74,44 +191,53 @@ namespace Scarlet.IO.ImageFormats
 
             PixelDataFormat pixelDataFormat, paletteDataFormat;
 
-            if (Format == 0x8602)
+            switch (ImageType)
             {
-                pixelDataFormat = PixelDataFormat.FormatIndexed4 | PixelDataFormat.PixelOrderingSwizzledPSP;
-                paletteDataFormat = PixelDataFormat.FormatAbgr4444;
-            }
-            else if (Format == 0x8803)
-            {
-                pixelDataFormat = PixelDataFormat.FormatIndexed4 | PixelDataFormat.PixelOrderingSwizzledPSP;
-                paletteDataFormat = PixelDataFormat.FormatAbgr8888;
-            }
-            else if (Format == 0x8A02)
-            {
-                pixelDataFormat = PixelDataFormat.FormatIndexed8 | PixelDataFormat.PixelOrderingSwizzledPSP;
-                paletteDataFormat = PixelDataFormat.FormatAbgr4444;
-            }
-            else if (Format == 0x8C03)
-            {
-                pixelDataFormat = PixelDataFormat.FormatIndexed8 | PixelDataFormat.PixelOrderingSwizzledPSP;
-                paletteDataFormat = PixelDataFormat.FormatAbgr8888;
-            }
-            else if (Format == 0x800A)
-            {
-                pixelDataFormat = PixelDataFormat.FormatDXT1Rgba_PSP;
-                paletteDataFormat = PixelDataFormat.Undefined;
-            }
-            else
-                throw new NotImplementedException();
+                // Non-indexed image
+                case GbixImageType.Linear:
+                case GbixImageType.LinearMipmaps:
+                    if (!dataTypeFormatMap.ContainsKey(DataType))
+                        throw new NotImplementedException(string.Format("GBIX: unimplemented pixel data type {0} (0x{1:X2})", DataType, (byte)DataType));
 
-            imageBinary.Width = Width;
-            imageBinary.Height = Height;
+                    pixelDataFormat = dataTypeFormatMap[DataType];
+                    if (!isCompressed) pixelDataFormat |= PixelDataFormat.PixelOrderingSwizzledPSP;
+                    paletteDataFormat = PixelDataFormat.Undefined;
+                    break;
+
+                // Indexed image (4-bit)
+                case GbixImageType.Indexed4:
+                case GbixImageType.Indexed4_Alt:
+                    if (!dataTypeFormatMap.ContainsKey(DataType))
+                        throw new NotImplementedException(string.Format("GBIX: unimplemented palette data type {0} (0x{1:X2})", DataType, (byte)DataType));
+
+                    pixelDataFormat = PixelDataFormat.FormatIndexed4 | PixelDataFormat.PixelOrderingSwizzledPSP;
+                    paletteDataFormat = dataTypeFormatMap[DataType];
+                    break;
+
+                // Indexed image (8-bit)
+                case GbixImageType.Indexed8:
+                case GbixImageType.Indexed8_Alt:
+                    if (!dataTypeFormatMap.ContainsKey(DataType))
+                        throw new NotImplementedException(string.Format("GBIX: unimplemented palette data type {0} (0x{1:X2})", DataType, (byte)DataType));
+
+                    pixelDataFormat = PixelDataFormat.FormatIndexed8 | PixelDataFormat.PixelOrderingSwizzledPSP;
+                    paletteDataFormat = dataTypeFormatMap[DataType];
+                    break;
+
+                default:
+                    throw new NotImplementedException(string.Format("GBIX: unimplemented image type {0} (0x{1:X2})", ImageType, (byte)ImageType));
+            }
+
+            imageBinary.Width = mipmapData[imageIndex].Width;
+            imageBinary.Height = mipmapData[imageIndex].Height;
             imageBinary.InputPaletteFormat = paletteDataFormat;
             imageBinary.InputPixelFormat = pixelDataFormat;
             imageBinary.InputEndianness = Endian.LittleEndian;
 
             imageBinary.AddInputPalette(PaletteData);
-            imageBinary.AddInputPixels(PixelData);
+            imageBinary.AddInputPixels(mipmapData[imageIndex].PixelData);
 
-            return imageBinary.GetBitmap();
+            return imageBinary.GetBitmap(0, 0);
         }
     }
 }
