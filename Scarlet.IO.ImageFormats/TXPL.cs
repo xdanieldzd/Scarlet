ae64afa8d6faa9cbe36f17fdb0111c3ffc9d56a4
xdanieldzd <xdanieldzd@googlemail.com>
2019-12-03 17:20:55 +0100

    Improved(?) TXPL support
    Now reads some more data & dumps both base images and individual elements

--- Scarlet.IO.ImageFormats/TXPL.cs
+++ Scarlet.IO.ImageFormats/TXPL.cs
@@ -5,58 +5,84 @@ using System.Text;
 using System.IO;
 using System.Drawing;
 
-using Scarlet.Drawing;
-
 namespace Scarlet.IO.ImageFormats
 {
+	// TODO: seems weird, not sure if correct
+
 	[MagicNumber("TXPL", 0x00)]
 	public class TXPL : ImageFormat
 	{
 		public string MagicNumber { get; private set; }
-		public uint Unknown1Offset { get; private set; }
-		public uint Unknown2Offset { get; private set; }
+		public uint HeaderSize { get; private set; }
+		public uint SubHeaderDataSize { get; private set; }
+		public TXPLImageInfo[] ImageInfos { get; private set; }
+		public uint FileSize { get; private set; }
 
-		public List<TXPLUnknownData1> UnknownData1s { get; private set; }
+		public TXPLSubHeader SubHeader { get; private set; }
 
-		List<GXT> gxtInstances;
+		List<Bitmap> baseImages, elementImages;
 
 		protected override void OnOpen(EndianBinaryReader reader)
 		{
 			MagicNumber = Encoding.ASCII.GetString(reader.ReadBytes(4));
-			Unknown1Offset = reader.ReadUInt32();
-			Unknown2Offset = reader.ReadUInt32();
+			HeaderSize = reader.ReadUInt32();
+			SubHeaderDataSize = reader.ReadUInt32();
 
-			UnknownData1s = new List<TXPLUnknownData1>();
-			while (true)
-			{
-				TXPLUnknownData1 unknownData = new TXPLUnknownData1(reader);
-				if ((unknownData.Unknown0x04 == 0 && unknownData.Unknown0x06 == 0) || (unknownData.ImageDataOffset == (uint)reader.BaseStream.Length)) break;
-				UnknownData1s.Add(unknownData);
-			}
+			long lastPosition = reader.BaseStream.Position;
+			reader.BaseStream.Position = HeaderSize;
+			SubHeader = new TXPLSubHeader(reader);
+			reader.BaseStream.Position = lastPosition;
 
-			gxtInstances = new List<GXT>();
-			for (int i = 0; i < UnknownData1s.Count; i++)
+			ImageInfos = new TXPLImageInfo[SubHeader.NumImageInfos];
+			for (int i = 0; i < ImageInfos.Length; i++) ImageInfos[i] = new TXPLImageInfo(reader);
+			FileSize = reader.ReadUInt32();
+
+			baseImages = new List<Bitmap>();
+
+			GXT gxtInstance = new GXT();
+			for (int i = 0; i < ImageInfos.Length; i++)
 			{
-				reader.BaseStream.Position = UnknownData1s[i].ImageDataOffset;
+				reader.BaseStream.Position = ImageInfos[i].ImageDataOffset;
 
 				using (MemoryStream gxtStream = new MemoryStream())
 				{
 					reader.BaseStream.CopyTo(gxtStream);
 					gxtStream.Position = 0;
 
-					GXT gxtInstance = new GXT();
 					gxtInstance.Open(gxtStream);
-
 					if (gxtInstance.TextureInfos.Length != 1) throw new Exception($"Unimplemented GXT image in TXPL; GXT has more than 1 TextureInfos ({gxtInstance.TextureInfos.Length})");
-
-					gxtInstances.Add(gxtInstance);
+					baseImages.Add(gxtInstance.GetBitmap());
 				}
+			}
+
+			elementImages = new List<Bitmap>();
+			for (int i = 0; i < SubHeader.Rectangles.Length; i++)
+			{
+				TXPLRectangle rectangle = SubHeader.Rectangles[i];
+
+				if (rectangle.Unknown0x00 != 0x00)
+				{
+					using (Bitmap bitmap = new Bitmap(rectangle.Width, rectangle.Height))
+					{
+						using (Graphics g = Graphics.FromImage(bitmap))
+						{
+							g.DrawImage(
+								baseImages[rectangle.ImageIndex],
+								new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+								rectangle.X, rectangle.Y, bitmap.Width, bitmap.Height,
+								GraphicsUnit.Pixel);
+						}
+						elementImages.Add((Bitmap)bitmap.Clone());
+					}
+				}
+				else
+					elementImages.Add(new Bitmap(32, 32));
 			}
 		}
 
 		public override int GetImageCount()
 		{
-			return gxtInstances.Count;
+			return (baseImages.Count + elementImages.Count);
 		}
 
 		public override int GetPaletteCount()
@@ -66,21 +92,62 @@ namespace Scarlet.IO.ImageFormats
 
 		protected override Bitmap OnGetBitmap(int imageIndex, int paletteIndex)
 		{
-			return gxtInstances[imageIndex].GetBitmap();
+			return (imageIndex < baseImages.Count ? baseImages[imageIndex] : elementImages[imageIndex - baseImages.Count]);
 		}
 	}
 
-	public class TXPLUnknownData1
+	public class TXPLSubHeader
+	{
+		public uint Unknown0x00 { get; private set; }
+		public uint Unknown0x04 { get; private set; }
+		public uint NumImageInfos { get; private set; }
+		public uint NumRectangles { get; private set; }
+
+		public TXPLRectangle[] Rectangles { get; private set; }
+
+		public TXPLSubHeader(EndianBinaryReader reader)
+		{
+			Unknown0x00 = reader.ReadUInt32();
+			Unknown0x04 = reader.ReadUInt32();
+			NumImageInfos = reader.ReadUInt32();
+			NumRectangles = reader.ReadUInt32();
+
+			Rectangles = new TXPLRectangle[NumRectangles];
+			for (int i = 0; i < Rectangles.Length; i++) Rectangles[i] = new TXPLRectangle(reader);
+		}
+	}
+
+	public class TXPLImageInfo
 	{
 		public uint ImageDataOffset { get; private set; }
 		public ushort Unknown0x04 { get; private set; }
 		public ushort Unknown0x06 { get; private set; }
 
-		public TXPLUnknownData1(EndianBinaryReader reader)
+		public TXPLImageInfo(EndianBinaryReader reader)
 		{
 			ImageDataOffset = reader.ReadUInt32();
 			Unknown0x04 = reader.ReadUInt16();
 			Unknown0x06 = reader.ReadUInt16();
+		}
+	}
+
+	public class TXPLRectangle
+	{
+		public ushort Unknown0x00 { get; private set; }
+		public ushort ImageIndex { get; private set; }
+		public ushort X { get; private set; }
+		public ushort Y { get; private set; }
+		public ushort Width { get; private set; }
+		public ushort Height { get; private set; }
+
+		public TXPLRectangle(EndianBinaryReader reader)
+		{
+			Unknown0x00 = reader.ReadUInt16();
+			ImageIndex = reader.ReadUInt16();
+			X = reader.ReadUInt16();
+			Y = reader.ReadUInt16();
+			Width = reader.ReadUInt16();
+			Height = reader.ReadUInt16();
 		}
 	}
 }
